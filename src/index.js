d9f4b409934996faa136937a9e201908ded70d67
kat2018 <katia.eidson@dcblox.com>
2018-10-08 17:07:09 -0400

    refactored Square and turned it into a function

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
+function Square(props) {
+  
     return (
     <button 
     className="square" 
-    onClick={() => this.props.onClick()}
+    onClick={props.onClick}
     >
-      {this.props.value}
+      {props.value}
     </button>
     );
-  }
 }
 
 class Board extends React.Component {
@@ -24,6 +23,12 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
     };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({squares: squares});
   }
 
   renderSquare(i) {
